6b7e9b10f91ecfb434c24abca9a00b6c88b7afbf
AdministradorCIFO <albertprofe@gmail.com>
2022-06-01 13:32:29 +0200

    Commit #2 : create project

--- library5/src/main/java/io/company/library/Book.java
+++ library5/src/main/java/io/company/library/Book.java
@@ -2,14 +2,45 @@ package io.company.library;
 
 //https://projectlombok.org/features/all
 import lombok.*;
+import javax.persistence.*;
 
-@Getter @Setter @ToString @NoArgsConstructor @AllArgsConstructor
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Entity(name="Book")
+@Table(name="BOOK_TABLE")
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="BOOK_ID")
+    private long bookId;
+    @Column(name="BOOK_TITLE")
     private String title;
+    @Column(name="AUTHOR")
     private String author;
     private int pages;
-    private int year;
-    private String ISBN;
+    @Column(name="PUBLISHED_YEAR")
+    private int publishedYear;
+    @Column(name="ISBN")
+    private String isbn;
 
+    //constructor without ID
+    public Book(String title, int pages, int publishedYear, String isbn) {
+        this.title = title;
+        this.pages = pages;
+        this.publishedYear = publishedYear;
+        this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookId=" + bookId +
+                ", title='" + title + '\'' +
+                ", authorName=" + author +
+                ", pages=" + pages +
+                ", publishedYear=" + publishedYear +
+                ", isbn='" + isbn + '\'' +
+                '}';
+    }
 }
--- library5/src/main/java/io/company/library/BookService.java
+++ library5/src/main/java/io/company/library/BookService.java
@@ -1,43 +1,51 @@
 package io.company.library;
 
-
-import java.util.ArrayList;
-import java.util.List;
-import com.github.javafaker.Faker;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
 
-    static List<Book> books = new ArrayList<Book>();
+    @Autowired
+    BookRepository bookRepository;
 
-    static {
-        //String title, String author, int pages, int year, String iSBN
-        Book book1 = new Book("Anna Karenina", "Tolstoy", 562, 1896, "BR5GV-5-ERG5-6567");
-        Book book2 = new Book("To the lighthouse", "V Wolf", 235, 1626, "56756-DFGRT-554");
-        Book book3 = new Book("I Love Java", "Anna Polinskaia", 256, 2023, "GWR-456");
-        //Book book4 = new Book ();
-        //Book book5 = new Book ();
-        //Book book6 = new Book ();
-        books.add(book3);
-        books.add(book2);
-        books.add(book1);
+    public Iterable<Book> getAllBooks() {
 
-        Faker faker = new Faker();
-
-       for (int i = 0; i <1000 ; i++ ){
-           books.add(new Book (faker.book().title(),faker.book().author(), faker.number().numberBetween(100, 1250),
-                   faker.number().numberBetween(1550, 2022),
-                   String.format(faker.number().digits(5).toString() + "-ERUMR-" +  faker.number().digits(5).toString() ) ));
-;       }
-
-    }
-
-    public List<Book> queryBooksFromArray() {
-
-        //System.out.println("Books" + books);
+        Iterable<Book> books = bookRepository.findAll();
 
         return books;
+    }
+
+    public Book createBook (Book book){
+
+        Book bookCreated = bookRepository.save(book);
+
+        return bookCreated;
+    }
+
+    public Optional<Book> findBookById(Long id){
+
+        return bookRepository.findById(id);
+    }
+
+    public Optional<Book> findBookByTitle(String title){
+        return bookRepository.findBookByTitle(title);
+    }
+
+    public Book deleteBookByTitle(String title){
+        //Find out IF this id-book IS in our DB
+        Optional<Book> deletedBook = bookRepository.deleteBookByTitle(title);
+        //
+        return null;
+    }
+
+    public void deleteBookById(Long id){
+        bookRepository.deleteById(id);
+    }
+
+    public Book updateBook (Book book){
+        return bookRepository.save(book);
     }
 
 
