3de1b373a6c8912bc2240ecd486b2a32c8ef4e2c
Albert <albertprofe@gmail.com>
2022-06-05 20:41:14 +0200

    Delete inherencejpa directory

--- inherencejpa/src/main/java/io/company/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package io.company.repository;
-
-import io.company.model.Author;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository extends CrudRepository<Author, Long> {
-}
--- inherencejpa/src/main/java/io/company/repository/PlayRepository.java
+++
@@ -1,8 +0,0 @@
-package io.company.repository;
-
-import io.company.model.Play;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PlayRepository extends CrudRepository<Play, Long> {
-
-}
